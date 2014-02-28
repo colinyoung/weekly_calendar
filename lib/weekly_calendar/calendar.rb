0be31971b73908dce8802b13577e72d4952c8ebc
Colin Young <me@colinyoung.com>
2014-02-28 14:12:17 -0800

    Add render of headings

--- lib/weekly_calendar/calendar.rb
+++ lib/weekly_calendar/calendar.rb
@@ -1,29 +1,29 @@
 module WeeklyCalendar
-  
+
   class Calendar
 
     attr_accessor :output_buffer
-    
+
     def self.as_html(options)
       self.new(options).to_html
     end
-    
+
     def initialize(options)
       @options = options
-      
+
       @view = options[:view]
       @date = options[:for] || options[:date] || Date.current
       @events = options[:events] || {}
     end
-    
+
     def to_s
       @days = days
-      
+
       content_tag :table, class: "weekly-calendar #{@options[:class]}", id: @options[:id] do
         content_tag(:thead, headings) + content_tag(:tbody, body)
       end
     end
-    
+
     def days
       start_week = (@date - 1.week)
       if ![0,6].include?(@date.wday) # is a weekday
@@ -35,20 +35,19 @@ module WeeklyCalendar
         start_week.week[1..5]
       end
     end
-    
+
     def headings
       content_tag :tr do
         String.new.html_safe.tap do |s|
           s << time_scale_heading if @options[:time_scale]
           @days.each do |d|
             # @todo Localize the month/day order
-            others = [ class: date_classes(d) ]
-            s << content_tag(:th, "#{Date::ABBR_DAYNAMES[d.wday]} #{d.month}/#{d.mday}", *others)
+            s << render_heading(d)
           end
         end
       end
     end
-    
+
     def body
       content_tag :tr do
         String.new.html_safe.tap do |s|
@@ -81,11 +80,11 @@ module WeeklyCalendar
     def date_box(date)
       content_tag :div, date.mday, class: 'wc-date'
     end
-    
+
     def events_for_date(date)
       events = @events.delete(date.to_date)
       return if events.nil?
-      
+
       content_tag :div, class: 'wc-events' do
         safe_str do |s|
           events.each do |event|
@@ -96,40 +95,46 @@ module WeeklyCalendar
         end
       end
     end
-    
+
     def events_ending_this_week(week, date)
       unless (events = events_ending_on(date)).empty?
         content_tag :div, class: 'wc-events-ending' do
-          safe_str do |s|            
+          safe_str do |s|
             events.compact.each do |event|
-              next if event.start_at_date >= week.first.to_date              
+              next if event.start_at_date >= week.first.to_date
               s << render_event(event, on: date.to_date)
             end
           end
         end
       end
     end
-    
+
     def to_html
       to_s.html_safe
     end
-    
+
     def safe_str(&block)
       String.new.html_safe.tap(&block)
     end
-    
+
     def content_tag(*args, &block)
       @view.content_tag(*args, &block)
     end
-    
+
     protected
-    
+
     def render_event(event, options={})
       content_tag(:div, class: "wc-event-container wc-event-duration-#{event.duration} wc-event-offset-#{event.start_time_offset}  wc-days-#{[event.days(options[:on] || options[:date]), 5].min}") do
         @view.render 'weekly_calendar/event', event: event
       end
     end
-    
+
+    def render_heading(date, options={})
+      content_tag(:th, class: date_classes(date)) do
+        @view.render 'weekly_calendar/heading', event: date
+      end
+    end
+
     def events_ending_on(date)
       Array.new.tap do |a|
         @events.each do |date,events|
@@ -139,13 +144,13 @@ module WeeklyCalendar
         end
       end
     end
-    
+
     def date_classes(date)
       Array.new.tap do |k|
         k << "today" if date.same_day_as?(Date.current)
         k << "past" if date.past?
       end
     end
-    
+
   end
 end
